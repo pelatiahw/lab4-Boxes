6a7de4a78451054ac3a4e42acd0c6f85211675de
InnovationBham_4 <InnovationBham_4@Admins-iMac.local>
2017-06-20 15:16:04 -0500

    commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var btn = document.createElement("BUTTON");
 var i = document.createTextNode("CLICK ME");
-btn.appenedChild[i];
+btn.appendChild(i);
 document.body.appendChild(btn);
 
-btn.onclick = function box () {
+btn.onclick = function box() {
     var div = document.createElement("div");
     div.className = "black";
     document.body.appendChild(div);
